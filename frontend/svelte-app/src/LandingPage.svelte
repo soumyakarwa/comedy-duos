4a145398f17dbce52f78971e704c71527023c21f
soumyakarwa <soumyakarwa01@gmail.com>
2024-06-02 15:03:26 -0400

    created landing page

---
+++ frontend/svelte-app/src/LandingPage.svelte
@@ -0,0 +1,58 @@
+<script>
+</script>
+
+<style>
+  .landing-page-container {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background-color: white;
+  }
+
+  .fixed-image {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 60vw; /* Adjust this size as needed */
+    height: auto;
+  }
+
+  .title {
+    font-size: 5.5rem;
+    font-family: "Impact"; 
+    margin-bottom: 1.4rem; 
+  }
+
+  .subtitle {
+    font-size: 9.5rem;
+    color: var(--yellow); /* Gold color */
+    font-family: "Impact";
+    line-height: 0.6; 
+  }
+
+  .hel {
+    font-size: 7.32rem; 
+    font-family: "Helvetica Neue";
+    font-weight: bold;
+  }
+
+  .author {
+    font-size: 1.25rem;
+    margin-top: 2.8rem;
+  }
+</style>
+
+
+<div class="landing-page-container">
+  <img src="/assets/cautionTape.svg" alt="Crime Scene Tape" class="fixed-image" />
+  <div>
+    <div class="title">THE MOST ICONIC DUO IN</div>
+    <div class="subtitle">BROOKLYN<br><span class="hel">NINE-NINE</span><br>
+    </div>
+    <div class="author">by Soumya Karwa</div>
+  </div>
+</div>
