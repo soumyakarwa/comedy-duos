3743e0c9c6e074571e069f2df7c4e50a0da43a3b
soumyakarwa <soumyakarwa01@gmail.com>
2024-06-03 09:31:45 -0400

    somehow figured the image placement for the character scrolly

--- frontend/svelte-app/src/Scrolly.svelte
+++ frontend/svelte-app/src/Scrolly.svelte
@@ -33,10 +33,14 @@
       bind:offset
       bind:progress
     >
-      <div slot="background" style="background-image: url({backgroundImage}); background-size: cover;">
+      <!-- <div slot="background" style="background-image: url({backgroundImage}); background-size: cover;">
+      </div> -->
+      <div slot="background" style="padding: 0 0 0 0;">
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+          <img src={backgroundImage} alt="show character image">        
       </div>
   
-      <div slot="foreground" style="padding: 0 0 0 50%;">
+      <div slot="foreground" style="padding: 0 25% 0 25%;">
         <section>If you don’t already know what Brooklyn Nine–Nine is (which is borderline ridiculous btw), let me bring you up to speed on one of the most iconic sitcoms of our time. A Golden Globe winner, Brooklyn Nine–Nine is a 2013–2021 workplace sitcom about Brooklyn’s 99th Precinct’s detective squad when a rule-following, outwardly-unemotional, highly decorated NYPD Captain Raymond Holt (played by Andre Braugher) takes over.</section>
         <section>Being the first openly Black Gay Police officer in the NYPD, Captain Holt has fought many uphill battles; but bringing the carefree, talented, almost irresponsible Detective Jacob Peralta (played by Andy Samberg) in line, might just be the toughest battle yet (lol I kid ofcourse). </section>
         <section>But what makes Brooklyn Nine–Nine so special is that it’s less about the crime and more about the people solving it. It’s about how their lives, their relationships, their ambitions change as a result of their experiences on the job. But set against a grim backdrop of a police precinct where more often than not, things don’t go as planned, it reminds us to find humor in life. As Jerry Seinfeld said, “Humor is the most powerful, most survival, essential quality, you will ever have or need to navigate through the human experience,” Brooklyn Nine–Nine captures viewers’ hearts by creating nuanced characters who are able to laugh at themselves and the situations they face. Reminding us “not enough of life makes sense” but “it can be survived through humor” and with the right people by your side.</section>
@@ -51,8 +55,16 @@
   <style>
     [slot="background"] {
       width: 100%; 
-      height: 90vh; 
-      transition: background-image 0.5s ease-in-out;
+      height: 100%; 
+      /* width: 100%;  */
+      /* height: 90vh;  */
+      /* bottom: 0px;  */
+    }
+
+    [slot="background"] img {
+      width: 100vw; 
+      height: auto;
+      /* transition: background-image 0.5s ease-in-out; */
       /* bottom: 0px;  */
     }
 
@@ -65,7 +77,7 @@
     }
     
     section {
-      height: 80vh;
+      height: 100vh;
       /* background-color: rgba(0,0,0,0.5); */
       color: black;
       padding: var(--margin);
