28b0ff88bcddb5e4c52a3dc6a3028d7b9b3a327f
soumyakarwa <soumyakarwa01@gmail.com>
2024-06-04 18:45:06 -0400

    Added epsiode square and figured out to show episode descriptions on scroll

--- frontend/svelte-app/src/EpisodeBreakdown.svelte
+++ frontend/svelte-app/src/EpisodeBreakdown.svelte
@@ -1,25 +1,34 @@
 <script>
-    import Scroll from "./Scrolly.svelte";
-    import { tweened } from "svelte/motion";
-    import * as d3 from 'd3';
-    import { onMount } from 'svelte';
+  import Scroll from "./Scrolly.svelte";
+  import { tweened } from "svelte/motion";
+  import * as d3 from 'd3';
+  import { onMount } from 'svelte';
+  import { writable } from 'svelte/store';
 
-    let episodeData = []; 
-    let specificDataPoint;
+  let episodeData = []; 
+  let specificDataPoint;
+  let svg; 
+  let width; 
+  let episodeDescriptions = []; 
 
-    onMount(async () => {
-    try {
-      episodeData = await d3.csv("/data/brooklynNineNineCharacters.csv");
-      specificDataPoint = episodeData.find(d => d.Title === "Hitchcock & Scully");
-      console.log("Data loaded:", episodeData);
-      console.log("Specific Data Point:", specificDataPoint);
-    } catch (error) {
-      console.error("Error loading data:", error);
-    }
-    });
-    console.log(episodeData); 
+  const showDescriptions = writable(false);
 
-    let data = [
+  onMount(async () => {
+      try {
+          episodeData = await d3.csv("/data/brooklynNineNineCharacters.csv");
+          specificDataPoint = episodeData.find(d => d.Title === "Hitchcock & Scully");
+          console.log("Data loaded:", episodeData);
+          episodeDescriptions = [specificDataPoint[`Episode Description`], specificDataPoint[`Wiki Fandom Descriptions`], specificDataPoint[`Wikipedia Episode Descriptions`]];
+      } catch (error) {
+          console.error("Error loading data:", error);
+      }
+
+      const svgRect = svg.getBoundingClientRect();
+      width = svgRect.width;
+      svg.setAttribute("height", width);
+  });
+
+  let data = [
       { foo: 4, bar: 1 },
       { foo: 6, bar: 7 },
       { foo: 9, bar: 5 },
@@ -29,91 +38,112 @@
       { foo: 5, bar: 3 },
       { foo: 3, bar: 8 },
       { foo: 1, bar: 6 },
-    ];
-    
-    const tweenedX = tweened(data.map((d) => d.foo));
-  
-    const setFoo = function () {
-      tweenedX.set(data.map((d) => d.foo));
-    };
-    const setBar = function () {
+  ];
+
+  const tweenedX = tweened(data.map((d) => d.foo));
+
+  const setBar = function () {
       tweenedX.set(data.map((d) => d.bar));
-    };
-  
-    let width = 400;
-    let height = 400;
-  
-    let xScale = d3.scaleLinear().domain([0, 10]).range([0, width]);
-    let yScale = d3.scaleLinear().domain([0, 10]).range([height, 0]);
-  
-    let currentStep;
-    const steps = [`Let's consider this square to represent an episode.`, `Step 1?`, `Step 2.`];
-  
-    $: if (currentStep == 0) {
-      setFoo();
-    } else if (currentStep == 1) {
-      setBar();
-    } else if (currentStep == 2) {
-      setFoo();
-    }
-  </script>
-  
-  <section class="episode-section">
-    <!-- The scrolling container which includes each of the text elements -->
-    <Scroll bind:value={currentStep}>
-        {#each steps as text, i}
-            <div class="step" class:active={currentStep === i}>
-                <div class="step-content">
-                    {@html text}
-                </div>
-            </div>
-        {/each}
-    </Scroll>
+  };
 
-    <div class="chart">
-        <svg {width} {height}>
-          {#each data as d, index}
-            <circle
-              cx={xScale($tweenedX[index])}
-              cy={yScale(d.bar)}
-              r={15}
-              fill="#438A00"
-            />
-          {/each}
-        </svg>
-    </div>  
-  </section>
-  
-  
-  <style>
+  let currentStep;
+  const steps = [`Let's consider this square to represent an episode.`, `As I mentioned previously, here are the three epsiode Descriptions, for Season 7, Episode 6 titled "Hitchcock & Scully."`, `Step 2.`];
 
-    .episode-section {
-        display: flex;
-        justify-content: center; 
-        gap: calc(var(--margin)*2)
-        /* height: 100vh;  */
-    }
+  $: if (currentStep == 1 || currentStep == 2) {
+      showDescriptions.set(true);
+  } else {
+      showDescriptions.set(false);
+      if (currentStep == 0) {
+          setBar();
+      }
+  }
+</script>
 
-    /* The fixed chart */
-    .chart {
-        /* background: #ffffff; */
-        width: 50vh;
-        height: 50vh;
-        position: sticky;
-        border: 1px solid var(--black); 
-        top: 25vh;
-    }
-  
-    /* Scrollytelling CSS */
-    .step {
+<section class="episode-section">
+  <!-- The scrolling container which includes each of the text elements -->
+  <Scroll bind:value={currentStep}>
+      {#each steps as text, i}
+          <div class="step" class:active={currentStep === i}>
+              <div class="step-content">
+                  {@html text}
+              </div>
+          </div>
+      {/each}
+  </Scroll>
+
+  <div class="chart">
+      <div id="col1">
+          <div class="episodeDescriptions" class:active={$showDescriptions}>{episodeDescriptions[0]}</div>
+          <svg bind:this={svg}>
+              {#if width > 0}
+                  <rect x={0} y={0} width={width} height={width} fill="#438A00"></rect>
+              {/if}
+          </svg>
+          <div class="episodeDescriptions" class:active={$showDescriptions}>{episodeDescriptions[1]}</div>
+      </div>
+      <div id="col2">
+          <div class="episodeDescriptions" class:active={$showDescriptions}>{episodeDescriptions[2]}</div>
+      </div>
+  </div>  
+</section>
+
+<style>
+  .episode-section {
+      display: flex;
+      justify-content: space-evenly; 
+      gap: var(--margin)
+  }
+
+  /* The fixed chart */
+  .chart {
+      width: 40vw;
+      height: 80vh;
+      position: sticky;
+      /* border: 1px solid var(--black);  */
+      top: 10vh;
+      display:flex; 
+      flex-direction: row;
+      align-items: center;
+      gap: var(--margin);
+      padding-left:calc(var(--margin)*2); 
+  }
+
+  #col1 {
+      display: flex; 
+      flex-direction: column;
+      gap: calc(var(--margin)/2); 
+      max-width: 50%; 
+  }
+
+  #col2 {
+      max-width: 50%; 
+  }
+
+  .episodeDescriptions {
+      opacity: 0;
+      transition: opacity 500ms ease;
+  }
+
+  .episodeDescriptions.active {
+      opacity: 1;
+  }
+
+  svg {
+      width: 100%;
+      /* height: 100%; */
+  }
+
+  /* Scrollytelling CSS */
+  .step {
       height: 60vh;
       max-width: 35vw; 
       display: flex;
       place-items: center;
       justify-content: center;
-    }
-  
-    .step-content {
+  }
+
+  .step-content {
+      /* max-width: 35vw;  */
       background: var(--white);
       opacity: 0.2; 
       color: var(--black);
@@ -123,9 +153,9 @@
       justify-content: center;
       transition: background-color 500ms ease;
       z-index: 10;
-    }
-  
-    .step.active .step-content {
+  }
+
+  .step.active .step-content {
       opacity: 1;  
-    }
-  </style>
+  }
+</style>
