8a1a69e2b02aaeb79a1323831385789acd13d2ee
soumyakarwa <soumyakarwa01@gmail.com>
2024-06-13 12:48:56 -0400

    helper function for rating bar chart. transitions complete

--- frontend/svelte-app/src/HeatMap.svelte
+++ frontend/svelte-app/src/HeatMap.svelte
@@ -3,9 +3,9 @@
   import { onMount } from 'svelte';
   import * as d3 from 'd3';
   import * as Constants from "./Constants.js"; 
-  import { group_outros } from 'svelte/internal';
 
-  let sectionTexts = [`Continuing from previous analysis.`, `In the previous section we analysed Season 3, Episode 6, "Into the woods". Let's put the anlaysis onto a grid.`, `Conducting the same analysis for all episodes.`, `To streamline, let’s consider pairs with top 10% pairings together.`, `Considering only frequency, we can tell that the pair with the most screen time is Captain Holt and Jake.`];
+  let sectionTexts = [`Continuing from previous analysis.`, `In the previous section we analysed Season 3, Episode 6, "Into the woods". Let's put the anlaysis onto a grid.`, `Conducting the same analysis for all episodes.`, `To streamline, let’s consider pairs with top 10% pairings together.`, `Now, there are two ways of finding the most iconic duo. The easiest, is to see which pair got the most screentime.`,
+  `To no one's surprise, we can see that the pair with the most appearences together iss DUN DUN DUN Jake & Amy.`, `In another approach, (perhaps more accurate) let's account for the number of votes and average rating of each episode to calculate the average cummilative rating.`];
   let count;
   let index = 0;
   let offset;
@@ -23,7 +23,7 @@
   const svgWidth = 0.9 * window.innerWidth;
   const svgHeight = 0.9 * window.innerHeight;
   
-  let svg, g, originalXScale, originalYScale, legend, chartWidth, chartHeight, frequencyXScale, frequencyYScale, topPairs, frequencies, pairToColor;
+  let svg, g, originalXScale, originalYScale, legend, chartWidth, chartHeight, frequencyXScale, frequencyYScale, topPairs, frequencies, pairToColor, ratingXScale, ratingYScale, ratings;
   const margin = { top: 100, right: 20, bottom: 50, left: 50 };
 
   $: if(episodeData.length) {
@@ -99,13 +99,14 @@
             frequency,
             averageCumulativeRating,
             averageRating,
+            totalVotes,
             ratingDifference: averageCumulativeRating - averageRating
         };
     }).sort((a, b) => b.frequency - a.frequency); 
 
-    // topPairs = sortedCharacterRatingArray.slice(0, 10).map(d => d.pair.join(' & '));
     topPairs = sortedCharacterRatingArray.slice(0, 10).map(d => pairToString(d.pair));
     frequencies = sortedCharacterRatingArray.slice(0, 10).map(d => d.frequency);
+    ratings = sortedCharacterRatingArray.slice(0, 10).map(d => d.averageCumulativeRating);
 
     frequencyXScale = d3.scaleBand()
         .domain(topPairs)
@@ -121,8 +122,13 @@
     topPairs.forEach((pair, index) => {
         pairToColor.set(pair, Constants.colors[index % 5]);
     });
-    console.log(pairToColor); 
 
+    ratingXScale = frequencyXScale;
+
+    // Create yScale for frequencies
+    ratingYScale = d3.scaleLinear()
+        .domain([0, d3.max(ratings)])
+        .range([chartHeight, margin.top/2]);
 
   };
 
@@ -183,13 +189,35 @@
    * Helper function when index == 1
    */
   function showSpecificSquare() {
-    g.selectAll('rect')
+    // REMOVING ALL PREVIOUS ADDITIONS
+    g.selectAll('.specific-square')
       .transition()
       .duration(Constants.transitionTime)
       .style('opacity', 0)
       .attr('width', 0)
-      .remove(); // Clear existing elements
+      .remove(); 
 
+    g.selectAll('.legend')
+      .transition()
+      .duration(Constants.transitionTime)
+      .style('opacity', 0)
+      .remove();
+
+    g.selectAll('.row-group')
+      .transition()
+      .duration(Constants.transitionTime)
+      .style('opacity', 0)
+      .attr('width', 0)
+      .remove(); 
+    
+    g.selectAll('.square rect')
+      .transition()
+      .duration(Constants.transitionTime)
+      .style('opacity', 0)
+      .attr('width', 0)
+      .remove(); 
+
+    // ADDING ONLY THE SQUARE CORRESPONDING TO SEASON 3, EPISODE 6
     const specificRect = g.append('rect')
       .attr('class', 'specific-square')
       .attr('x', originalXScale(specificDataPoint.Episode))
@@ -203,6 +231,7 @@
 
     for (let i = 0; i < numRows; i++) {
       g.append('rect')
+        .attr('class', 'specific-square')
         .attr('x', +specificRect.attr('x'))
         .attr('y', +specificRect.attr('y') + i * rowHeight)
         .attr('width', 0)
@@ -221,19 +250,35 @@
    * Helper function when index == 2
    */
   function showAllSquares() {
+    // REMOVING ALL PREVIOUSLY ADDED 
+    g.selectAll('.specific-square')
+      .transition()
+      .duration(Constants.transitionTime)
+      .style('opacity', 0)
+      .attr('width', 0)
+      .remove();
+
     g.selectAll('.legend')
       .transition()
       .duration(Constants.transitionTime)
       .style('opacity', 0)
       .remove();
 
-    g.selectAll('rect')
+    g.selectAll('.row-group')
+      .transition()
+      .duration(Constants.transitionTime)
+      .style('opacity', 0)
+      .attr('width', 0)
+      .remove(); 
+    
+    g.selectAll('.square rect')
       .transition()
       .duration(Constants.transitionTime)
       .style('opacity', 0)
       .attr('width', 0)
       .remove(); 
 
+    // ADDING BASE SQUARES FOR EACH EPISODE
     const squares = g.append('g')
       .selectAll('.square')
       .data(episodeData)
@@ -282,12 +327,13 @@
    *
    */
   function updateSquaresForTopPairs() {
-    g.selectAll('.frequency-bar rect')
-        .transition()
-        .duration(Constants.transitionTime)
-        .style('opacity', 0)
-        .attr('width', 0)
-        .remove();
+    g.selectAll('.frequency-bar')
+      .transition()
+      .duration(Constants.transitionTime)
+      .style('opacity', 0)
+      .attr('y', chartHeight) 
+      .attr('height', 0)
+      .remove();
 
     const currentDomain = g.select('.axis-x').selectAll('.tick').data();
 
@@ -331,7 +377,7 @@
           .attr('height', (d, i, nodes) => originalXScale.bandwidth() / nodes.length)
           .style('fill', d => {
               const pair = d.char;
-              return pairToColor.get(pairToString(pair)) || 'grey'; // Default color if not found
+              return pairToColor.get(pairToString(pair)) || Constants.yellowColor;
           });
       });
 
@@ -349,6 +395,7 @@
       .transition()
       .duration(Constants.transitionTime)
       .style('opacity', 0)
+      .attr('width', 0)
       .remove();
 
     // Create legend
@@ -386,93 +433,108 @@
   }
 
   /**
-   * 
+   * Helper function when index == 4
    */
   function createStackedBarChart(){
-    g.selectAll('.legend')
+    g.selectAll('.frequency-bar')
       .transition()
-      .duration(Constants.transitionTime*2)
+      .duration(Constants.transitionTime)
       .style('opacity', 0)
+      .attr('y', chartHeight) 
+      .attr('height', 0)
       .remove();
 
-    // const topPairs = sortedCharacterRatingArray.slice(0, 10).map(d => d.pairToString(pair));
-    // const frequencies = sortedCharacterRatingArray.slice(0, 10).map(d => d.frequency);
-
-    // Create xScale for pairs
-    // const newX = d3.scaleBand()
-    //     .domain(topPairs)
-    //     .range([0, chartWidth])
-    //     .padding(0.1);
-
-    // // Create yScale for frequencies
-    // const newY = d3.scaleLinear()
-    //     .domain([0, d3.max(frequencies)])
-    //     .range([chartHeight, margin.top/2]);
+    g.selectAll('.legend')
+      .transition()
+      .duration(Constants.transitionTime)
+      .style('opacity', 0)
+      .remove();
 
     // Update x-axis
     g.select('.axis-x')
         .transition()
-        .duration(Constants.transitionTime*2)
+        .duration(Constants.transitionTime)
         .call(d3.axisBottom(frequencyXScale))
         .attr('transform', `translate(0,${chartHeight})`);
+
+        // Update x-axis label
+     g.select(".x-axis-label")
+        .transition()
+        .duration(Constants.transitionTime)
+        .attr("y", chartHeight + margin.top/4+15)
+        .text("Top Character Pairs");
 
     // Update y-axis with transition
     g.select('.axis-y')
         .transition()
-        .duration(Constants.transitionTime*2)
+        .duration(Constants.transitionTime)
         .call(d3.axisLeft(frequencyYScale));
-
-     // Update x-axis label
-     g.select(".x-axis-label")
-        .transition()
-        .duration(Constants.transitionTime*2)
-        .attr("y", chartHeight + margin.top/4+15)
-        .text("Top Character Pairs");
-
+     
     // Update y-axis label
     g.select(".y-axis-label")
         .transition()
-        .duration(Constants.transitionTime*2)
+        .duration(Constants.transitionTime)
         .attr("transform", `translate(${-margin.left/2-5},${chartHeight/2}) rotate(-90)`)
         .text("Frequency"); 
 
     // Move the remaining rects to the appropriate position in the new scale
-    g.selectAll('.row-group')
+      g.selectAll('.row-group')
         .selectAll('rect')
         .filter(function(d) {
             const pair = d.char;
             return topPairs.some(topPair => topPair === pairToString(pair));
         })
         .transition()
-        .duration(Constants.transitionTime*2)
+        .duration(Constants.transitionTime)
         .attr('x', d => frequencyXScale(pairToString(d.char)) + frequencyXScale.bandwidth()/4)
         .attr('y', chartHeight - 11)
         .attr('width', frequencyXScale.bandwidth()/2 - 2)
         .attr('height', 10)
         .on('end', function(d, i) {
-          addFrequencyBars();  // Only call once after the last transitio
-        });
+          addFrequencyBars(); 
+          });
+
 
     function addFrequencyBars() {
-        g.selectAll('.frequency-bar')
-            .data(sortedCharacterRatingArray.slice(0, 10))
-            .enter().append('rect')
-            .attr('class', 'frequency-bar')
-            .attr('x', d => frequencyXScale(pairToString(d.pair)) + frequencyXScale.bandwidth()/4)
-            .attr('y', chartHeight) // Start from the bottom
-            .attr('width', frequencyXScale.bandwidth()/2)
-            .attr('height', 0) // Initially set to 0
-            .style('fill', d => {
-              console.log(d.pair); 
-              console.log(pairToColor.get(pairToString(d.pair))); 
-              return pairToColor.get(pairToString(d.pair));
-            })
-            .transition()
-            .duration(Constants.transitionTime)
-            .attr('y', d => frequencyYScale(d.frequency))
-            .attr('height', d => chartHeight - frequencyYScale(d.frequency)); // Adjust height based on frequency
+      g.selectAll('.frequency-bar')
+        .data(sortedCharacterRatingArray.slice(0, 10))
+        .enter().append('rect')
+        .attr('class', 'frequency-bar')
+        .attr('x', d => frequencyXScale(pairToString(d.pair)) + frequencyXScale.bandwidth()/4)
+        .attr('y', chartHeight) 
+        .attr('width', frequencyXScale.bandwidth()/2)
+        .attr('height', 0) 
+        .style('fill', d => pairToColor.get(pairToString(d.pair)))
+        .transition()
+        .duration(Constants.transitionTime)
+        .attr('y', d => frequencyYScale(d.frequency))
+        .attr('height', d => chartHeight - frequencyYScale(d.frequency));
     }
-}
+  }
+
+  /**
+   * Helper function when index == 6
+   */
+  function createRatingBarChart(){
+    // Update y-axis with transition
+    g.select('.axis-y')
+        .transition()
+        .duration(Constants.transitionTime)
+        .call(d3.axisLeft(ratingYScale));
+     
+    // Update y-axis label
+    g.select(".y-axis-label")
+        .transition()
+        .duration(Constants.transitionTime)
+        // .attr("transform", `translate(${-margin.left/2-5},${chartHeight/2}) rotate(-90)`)
+        .text("Rating"); 
+
+    g.selectAll('.frequency-bar')
+        .transition()
+        .duration(Constants.transitionTime)
+        .attr('y', d => ratingYScale(d.averageCumulativeRating))
+        .attr('height', d => chartHeight - ratingYScale(d.averageCumulativeRating)); 
+  }
 
   // Call the updateHeatMap function based on the index value
   $: if(g) {
@@ -484,6 +546,8 @@
       updateSquaresForTopPairs();
     } else if (index == 4) {
       createStackedBarChart();
+    } else if (index == 6) {
+      createRatingBarChart();
     }
   }
 
