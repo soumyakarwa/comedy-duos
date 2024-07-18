37439343454fe2363868358f59092db6a83c85d2
soumyakarwa <soumyakarwa01@gmail.com>
2024-07-18 11:37:10 -0400

    landing page lines and thumpins are now responsive...git add .

--- frontend/svelte-app/src/LandingPage.svelte
+++ frontend/svelte-app/src/LandingPage.svelte
@@ -6,6 +6,7 @@
   
   let landingPageSvg;
   let svgWidth, svgHeight; 
+  let initialSetup = true;
   export let sectionIndex;
   let caseImg, detectiveImg, arrowKeyImg, titleImg;  
 
@@ -25,8 +26,8 @@
     width: 0,
     x: 0,
     y: 0,
-    bottomPins: null,
-    topPins: null
+    topPins: {ellipse: null, pos: null}, 
+    bottomPins: {ellipse: null, pos: null}
   };
 
   let arrowImgData = {
@@ -35,8 +36,8 @@
     width: 0,
     x: 0,
     y: 0,
-    bottomPins: null,
-    topPins: null
+    topPins: {ellipse: null, pos: null}, 
+    bottomPins: {ellipse: null, pos: null}
   };
 
   let titleImgData = {
@@ -45,10 +46,16 @@
     width: 0,
     x: 0,
     y: 0,
-    bottomPins: null,
-    topPins: null
+    topPins: [{ellipse: null, pos: null}, {ellipse: null, pos: null}],
+    bottomPins: [{ellipse: null, pos: null}, {ellipse: null, pos: null}]
   };
 
+  let lines = {
+    topLeft: {line: null, startingPos: null, endingPos: null},
+    topRight: {line: null, startingPos: null, endingPos: null},
+    bottomLeft: {line: null, startingPos: null, endingPos: null},
+    bottomRight: {line: null, startingPos: null, endingPos: null},
+  }
 
   function setPosition(svg, svgWidth, svgHeight){
     caseImageData.height = svgHeight * 0.07;
@@ -60,109 +67,164 @@
     detectiveImgData.height = svgHeight * 0.09;
     detectiveImgData.x = svgWidth*0.88; 
     detectiveImgData.y = svgHeight * 0.88; 
-    detectiveImgData.topPins = [detectiveImgData.x + svgWidth*0.047, detectiveImgData.y];
+    detectiveImgData.topPins.pos = [detectiveImgData.x + svgWidth*0.047, detectiveImgData.y];
 
     titleImgData.height = svgHeight * 0.46;
     titleImgData.width = 1011/435 * titleImgData.height; 
     titleImgData.x = svgWidth*0.215; 
     titleImgData.y = svgHeight * 0.285; 
-    titleImgData.topPins = [
-      [titleImgData.x + 0.3*titleImgData.width, titleImgData.y], 
-      [titleImgData.x+ titleImgData.width - 4, titleImgData.y]
-    ]; 
-    titleImgData.bottomPins = [
-      [titleImgData.x + 0.2*titleImgData.width, titleImgData.y + titleImgData.height],
-      [titleImgData.x + titleImgData.width - 6, titleImgData.y + titleImgData.height]
-    ];
+    titleImgData.topPins[0].pos = [titleImgData.x + 0.3*titleImgData.width, titleImgData.y]; 
+    titleImgData.topPins[1].pos = [titleImgData.x+ titleImgData.width - 4, titleImgData.y];
+    titleImgData.bottomPins[0].pos = [titleImgData.x + 0.2*titleImgData.width, titleImgData.y + titleImgData.height]; 
+    titleImgData.bottomPins[1].pos = [titleImgData.x + titleImgData.width - 6, titleImgData.y + titleImgData.height]; 
 
     arrowImgData.height = titleImgData.height * 0.2; 
     arrowImgData.width = arrowImgData.height*169/88.41; 
     arrowImgData.x = titleImgData.x + titleImgData.width + 10; 
     arrowImgData.y = titleImgData.y + titleImgData.height - arrowImgData.height; 
-    arrowImgData.topPins = [arrowImgData.x + arrowImgData.width/2, arrowImgData.y];
+    arrowImgData.topPins.pos = [arrowImgData.x + arrowImgData.width/2, arrowImgData.y];
+
+  }
+
+  function addImages(svg){
+    if(!caseImageData.img){
+      caseImageData.img = svg.append("image")
+        .attr("xlink:href", "/assets/landing-page/caseTextWithBorder.svg") 
+    }
+
+    if(!titleImgData.img){
+      titleImgData.img = svg.append("image")
+       .attr("xlink:href", "/assets/landing-page/titleWithBorder.svg") ; 
+    }
+
+    if(!detectiveImgData.img){
+      detectiveImgData.img = svg.append("image")
+      .attr("xlink:href", "/assets/landing-page/detectiveWithBorder.svg") 
+    }
+
+    if(!arrowImgData.img){
+        arrowImgData.img = svg.append("image")
+        .attr("xlink:href", "/assets/landing-page/arrowsWithBorder.svg")
+        .attr("opacity", 0); 
+      }
 
   }
 
   function setup(svg, svgWidth, svgHeight) {
 
     setPosition(svg, svgWidth, svgHeight);
-   
-    if(!caseImageData.img){
-      caseImageData.img = svg.append("image")
-        .attr("xlink:href", "/assets/landing-page/caseTextWithBorder.svg") 
-    }
+    addImages(svg); 
     
     caseImageData.img.attr("x", caseImageData.x) 
         .attr("y", caseImageData.y)
         .attr("height", caseImageData.height);
 
-    if(!titleImgData.img){
-      titleImgData.img = svg.append("image")
-       .attr("xlink:href", "/assets/landing-page/titleWithBorder.svg") ; 
-    }
     titleImgData.img
        .attr("x", titleImgData.x) 
        .attr("y",titleImgData.y)
       //  .attr("width", titleImgWidth);   
        .attr("height", titleImgData.height);
 
-    if(!detectiveImgData.img){
-      detectiveImgData.img = svg.append("image")
-      .attr("xlink:href", "/assets/landing-page/detectiveWithBorder.svg") 
-    }
     detectiveImgData.img
     .attr("x", detectiveImgData.x) 
     .attr("y", detectiveImgData.y)
     .attr("height", detectiveImgData.height); 
 
+    if (initialSetup) {
     setTimeout(() => {
-      if(!arrowImgData.img){
-        arrowImgData.img = svg.append("image")
-        .attr("xlink:href", "/assets/landing-page/arrowsWithBorder.svg")
-      }
       arrowImgData.img
-        .attr("x", arrowImgData.x) 
+        .attr("x", arrowImgData.x)
         .attr("y", arrowImgData.y)
         .attr("height", arrowImgData.height)
         .attr("opacity", 0)
         .transition()
         .duration(Constants.transitionTime)
-        .attr("opacity", 1); 
-      createThumbPin(svg, arrowImgData.topPins); 
-    }, Constants.maxLineDelay*5);    
-   
-    createThumbPin(svg, caseImageData.topPins.pos); 
-    
+        .attr("opacity", 1);
+        addOrUpdateThumbPin(svg, arrowImgData.topPins);
+    }, Constants.maxLineDelay * 5);
+
+    addOrUpdateThumbPin(svg, caseImageData.topPins);
+
     setTimeout(() => {
-      createThumbPin(svg, caseImageData.bottomPins.pos); 
-      createThumbPin(svg, titleImgData.topPins[0]);    
+      addOrUpdateThumbPin(svg, caseImageData.bottomPins);
+      addOrUpdateThumbPin(svg, titleImgData.topPins[0]);
       setTimeout(() => {
-      createLine(svg, caseImageData.bottomPins.pos, titleImgData.topPins[0], 0)
-      }, Constants.maxLineDelay/2); 
-    }, Constants.maxLineDelay); 
-    
+        addOrUpdateLine(svg, lines.topLeft, caseImageData.bottomPins.pos, titleImgData.topPins[0].pos);
+      }, Constants.maxLineDelay / 2);
+    }, Constants.maxLineDelay);
+
     setTimeout(() => {
-      createThumbPin(svg, titleImgData.topPins[1]); 
+      addOrUpdateThumbPin(svg, titleImgData.topPins[1]);
       setTimeout(() => {
-        createLine(svg, titleImgData.topPins[1], [svgWidth, 0],  0)
-      }, Constants.maxLineDelay/2); 
-    }, Constants.maxLineDelay*2); 
-    
+        addOrUpdateLine(svg, lines.topRight, titleImgData.topPins[1].pos, [svgWidth, 0])
+      }, Constants.maxLineDelay / 2);
+    }, Constants.maxLineDelay * 2);
+
     setTimeout(() => {
-      createThumbPin(svg, titleImgData.bottomPins[0]);     
+      addOrUpdateThumbPin(svg, titleImgData.bottomPins[0]);
       setTimeout(() => {
-        createLine(svg, titleImgData.bottomPins[0], [svgWidth*0.455, svgHeight],  0)
-      }, Constants.maxLineDelay/2); 
-    }, Constants.maxLineDelay*4);
-    
+        addOrUpdateLine(svg, lines.bottomLeft,  titleImgData.bottomPins[0].pos, [svgWidth * 0.455, svgHeight]);
+      }, Constants.maxLineDelay / 2);
+    }, Constants.maxLineDelay * 4);
+
     setTimeout(() => {
-      createThumbPin(svg, titleImgData.bottomPins[1]);  
-      createThumbPin(svg, detectiveImgData.topPins);  
-      setTimeout(() => {    
-        createLine(svg, titleImgData.bottomPins[1], detectiveImgData.topPins,  0); 
-      }, Constants.maxLineDelay/2); 
-    }, Constants.maxLineDelay*3);
+      addOrUpdateThumbPin(svg, titleImgData.bottomPins[1]);
+      addOrUpdateThumbPin(svg, detectiveImgData.topPins);
+      setTimeout(() => {
+        addOrUpdateLine(svg, lines.bottomRight, titleImgData.bottomPins[1].pos, detectiveImgData.topPins.pos);
+      }, Constants.maxLineDelay / 2);
+    }, Constants.maxLineDelay * 3);
+
+      initialSetup = false; // Set the flag to false after the initial setup
+    } else {
+      arrowImgData.img
+        .attr("x", arrowImgData.x)
+        .attr("y", arrowImgData.y)
+        .attr("height", arrowImgData.height); 
+      addOrUpdateThumbPin(svg, arrowImgData.topPins);
+      addOrUpdateThumbPin(svg, caseImageData.topPins);
+      addOrUpdateThumbPin(svg, caseImageData.bottomPins);
+      addOrUpdateThumbPin(svg, titleImgData.topPins[0]);
+      addOrUpdateThumbPin(svg, titleImgData.topPins[1]);
+      addOrUpdateThumbPin(svg, titleImgData.bottomPins[0]);
+      addOrUpdateThumbPin(svg, titleImgData.bottomPins[1]);
+      addOrUpdateThumbPin(svg, detectiveImgData.topPins);
+
+      addOrUpdateLine(svg, lines.topLeft, caseImageData.bottomPins.pos, titleImgData.topPins[0].pos);
+      addOrUpdateLine(svg, lines.topRight, titleImgData.topPins[1].pos, [svgWidth, 0]); 
+      addOrUpdateLine(svg, lines.bottomLeft,  titleImgData.bottomPins[0].pos, [svgWidth * 0.455, svgHeight]);
+      addOrUpdateLine(svg, lines.bottomRight, titleImgData.bottomPins[1].pos, detectiveImgData.topPins.pos);
+    }
   }
+
+  function addOrUpdateThumbPin(svg, thumbPin){
+    if(!thumbPin.ellipse){
+      thumbPin.ellipse = createThumbPin(svg, thumbPin.pos); 
+    }
+    else {
+      // updateThumbPinPosition(caseImageData.topPins.ellipse, caseImageData.topPins.pos); 
+      thumbPin
+      .ellipse
+      .transition()
+      .duration(Constants.transitionTime)
+      .attr("cx", thumbPin.pos[0])
+      .attr("cy", thumbPin.pos[1]);
+    }
+  }
+
+  function addOrUpdateLine(svg, lineData, startPos, endPos) {
+  if (!lineData.line) {
+    lineData.line = createLine(svg, startPos, endPos, 0);
+  } else {
+    lineData.line
+      .transition()
+      .duration(Constants.transitionTime)
+      .attr("x1", startPos[0])
+      .attr("y1", startPos[1])
+      .attr("x2", endPos[0])
+      .attr("y2", endPos[1]);
+  }
+}
 
   onMount(async () => {
     const svg = d3.select(landingPageSvg);
--- frontend/svelte-app/src/Util.js
+++ frontend/svelte-app/src/Util.js
@@ -5,35 +5,40 @@ import * as d3 from "d3";
 export function createThumbPin(svg, pos) {
   const ellipse = svg
     .append("ellipse")
+    // .attr("class", "thumb-pin")
     .attr("rx", Constants.ellipseSize)
     .attr("ry", Constants.ellipseSize)
     .attr("cx", pos[0])
     .attr("cy", pos[1])
     .attr("fill", Constants.yellowColor)
-    .attr("opacity", 0)
-    .transition()
-    .duration(Constants.transitionTime)
-    .attr("opacity", 1);
+    .attr("opacity", 0);
+
+  ellipse.transition().duration(Constants.transitionTime).attr("opacity", 1);
 
   return ellipse;
 }
 
 export function createLine(svg, pos1, pos2, delay) {
-  svg
+  const line = svg
     .append("line")
+    // .attr("class", "line")
     .style("stroke", Constants.yellowColor)
     .style("stroke-width", 2)
     .style("opacity", 1)
     .attr("x1", pos1[0])
     .attr("y1", pos1[1])
     .attr("x2", pos1[0])
-    .attr("y2", pos1[1])
+    .attr("y2", pos1[1]);
+
+  line
     .transition()
     .delay(delay)
     .duration(Constants.transitionTime * 2)
     .ease(d3.easeSinInOut)
     .attr("x2", pos2[0])
     .attr("y2", pos2[1]);
+
+  return line;
 }
 
 export function setSvgDimensions(id, svg) {
