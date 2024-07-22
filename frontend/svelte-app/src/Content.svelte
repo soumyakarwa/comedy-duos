b5417eb6ccfdd04929693100b093d1c5e54b8d40
soumyakarwa <soumyakarwa01@gmail.com>
2024-07-22 15:57:50 -0400

    changed the specific episode to the oolong slayer one

--- frontend/svelte-app/src/Content.svelte
+++ frontend/svelte-app/src/Content.svelte
@@ -56,7 +56,7 @@
 			const response = await fetch("/data/brooklynNineNineCharactersStreamlined.json");
 			const text = await response.text();
 			episodeData = JSON.parse(text);
-			specificDataPoint = episodeData.find(d => d.Title === "The Funeral");
+			specificDataPoint = episodeData.find(d => d.Title === "The Oolong Slayer");
 		} catch (error) {
 			console.error("Error loading data:", error);
 		}
